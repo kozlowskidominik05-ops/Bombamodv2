75d14b4be48149407e51d32ff041f58209f0d253
kozlowskidominik05-ops <kozlowskidominik05@gmail.com>
2026-03-04 22:13:20 +0100

    Update BombaMod.java

--- src/main/java/com/twojanazwa/bombamod/BombaMod.java
+++ src/main/java/com/twojanazwa/bombamod/BombaMod.java
@@ -1,1 +1,31 @@
+package com.twojanazwa.bombamod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.minecraft.text.Text;
+
+public class BombaMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Rejestracja komendy /bomba <nick>
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("bomba")
+                .then(ClientCommandManager.argument("nick", StringArgumentType.string())
+                .executes(context -> {
+                    String victim = StringArgumentType.getString(context, "nick");
+                    
+                    // Wiadomość na czacie (widoczna tylko dla Ciebie)
+                    context.getSource().sendFeedback(Text.literal("§c[!] Inicjacja protokołu na: §f" + victim));
+                    context.getSource().sendFeedback(Text.literal("§7Wysyłanie pakietów ruchu..."));
+                    
+                    // Odpalenie silnika lagów z drugiego pliku
+                    ExploitLogic.startSpam();
+                    
+                    return 1;
+                })));
+        });
+    }
+}
 
